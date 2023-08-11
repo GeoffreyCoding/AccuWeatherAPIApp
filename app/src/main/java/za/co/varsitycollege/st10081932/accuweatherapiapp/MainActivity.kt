75bbfcc4108b798250fe1dc3545098526f89ba75
GeoffreyCoding <127305585+GeoffreyCoding@users.noreply.github.com>
2023-08-11 15:31:44 +0200

    Shows Json on the Text view, we were told to stop development at the moment...

--- app/src/main/java/za/co/varsitycollege/st10081932/accuweatherapiapp/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10081932/accuweatherapiapp/MainActivity.kt
@@ -2,10 +2,27 @@ package za.co.varsitycollege.st10081932.accuweatherapiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import za.co.varsitycollege.st10081932.accuweatherapiapp.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val networkUtil = NetworkUtil()
+
+        thread {
+            val weather = try {
+                networkUtil.buildURLForWeather()?.readText()
+            } catch (e: Exception) {
+                return@thread
+            }
+            runOnUiThread {
+                binding.tvWeather.text = weather
+            }
+        }
     }
 }
