cdfe0bcf71b01aba259d4b800f9c798d269eb412
GeoffreyCoding <127305585+GeoffreyCoding@users.noreply.github.com>
2023-08-11 14:59:42 +0200

    Initial commit

---
+++ app/src/main/java/za/co/varsitycollege/st10081932/accuweatherapiapp/NetworkUtil.kt
@@ -0,0 +1,10 @@
+package za.co.varsitycollege.st10081932.accuweatherapiapp
+
+class NetworkUtil {
+    private val WEATHERBASE_URL =
+        "https://dataservice.accuweather.com/forecasts/v1/daily/5day/305605"
+    private val PARAM_METRIC = "metric"
+    private val METRIC_VALUE = "true"
+    private val PARAM_API_KEY = "apikey"
+    private val LOGGING_TAG = "URLWECREATED"
+}
